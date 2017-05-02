de183c9d8e65c029097578ed6d616b0b8eae41bc
Benjamin Tran <benjamin.tran25@gmail.com>
2017-05-02 16:11:38 -0700

    [ FEAT ] Add action creators for handling post request to add and remove items from cart

---
+++ client/actions/index.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+
+export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
+export const FETCH_CART = 'FETCH_CART';
+export const REMOVE_ITEM_FROM_CART = 'REMOVE_ITEM_FROM_CART';
+export const ADD_ITEM_TO_CART = 'ADD_ITEM_TO_CART';
+export const REMOVE_ITEM_FROM_INVENTORY = 'REMOVE_ITEM_FROM_INVENTORY';
+export const ADD_ITEM_TO_INVENTORY = 'ADD_ITEM_TO_INVENTORY';
+
+export function getProductsList() {
+  const url = '/api/products';
+  const request = axios.get(url);
+  return (dispatch) => {
+    request.then(({ data }) => {
+      dispatch({ type: FETCH_PRODUCTS, payload: data });
+    });
+  };
+}
+
+export function getCartList() {
+  const url = '/api/cart';
+  const request = axios.get(url);
+  return (dispatch) => {
+    request.then(({ data }) => {
+      dispatch({ type: FETCH_CART, payload: data });
+    });
+  };
+}
+
+export function removeFromCart(itemId) {
+  const url = `api/remove?${itemId}`;
+  console.log('remove url', url);
+  const request = axios.post(url);
+  return (dispath) => {
+    request.then(({ data }) => {
+      console.log(data);
+      dispath({ type: ADD_ITEM_TO_INVENTORY, payload: data });
+    }).then(() => {
+      dispatch({ type: REMOVE_ITEM_FROM_CART, payload: itemId });
+    });
+  };
+}
+
+export function addToCart(itemId) {
+  const url = `api/add?${itemId}`;
+  console.log('add url', url);
+  const request = axios.post(url);
+  return (dispath) => {
+    request.then(({ data }) => {
+      console.log(data);
+      dispath({ type: ADD_ITEM_TO_CART, payload: data });
+    }).then(() => {
+      console.log('in here');
+      dispath({ type: REMOVE_ITEM_FROM_INVENTORY, payload: itemId });
+    });
+  };
+}
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,7 +1,56 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
+import { FETCH_PRODUCTS, FETCH_CART, REMOVE_ITEM_FROM_CART, ADD_ITEM_TO_CART, REMOVE_ITEM_FROM_INVENTORY, ADD_ITEM_TO_INVENTORY } from '../actions/index';
+
+function productsReducer(state = [], action) {
+  switch (action.type) {
+    case FETCH_PRODUCTS:
+    return action.payload || state;
+    case REMOVE_ITEM_FROM_INVENTORY:
+    return removeFromList(state, action);
+    case ADD_ITEM_TO_INVENTORY:
+    return addToList(state, action);
+    default:
+    return state;
+  }
+}
+function cartReducer(state = [], action) {
+  switch (action.type) {
+    case FETCH_CART:
+    return action.payload || state;
+    case REMOVE_ITEM_FROM_CART:
+    return removeFromList(state, action);
+    case ADD_ITEM_TO_CART:
+    return addToList(state, action);
+    default:
+    return state;
+  }
+}
+
+function addToList(prevState, action) {
+  const newState = [...prevState, action.payload];
+  console.log('newState:', newState);
+  return newState;
+}
+
+function removeFromList(prevState, action) {
+  const copiedState = [...prevState];
+  let index;
+  for (let i = 0; i < copiedState.length; i++) {
+    console.log('in reducer', action.payload);
+    if (copiedState[i]._id === action.payload) {
+      index = i;
+    }
+  }
+  copiedState.splice(index, 1);
+  return copiedState;
+}
+
+
 const rootReducer = combineReducers({
+  cartList: cartReducer,
+  productsList: productsReducer,
   router: routerReducer,
 });
 
