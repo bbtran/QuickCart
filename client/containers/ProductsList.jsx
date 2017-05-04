ee2aa58045743091b7073256a790288ba8e9e69b
Benjamin Tran <benjamin.tran25@gmail.com>
2017-05-03 22:14:26 -0700

    [ BUGFIX ] toggle button disable on click to prevent server crashing

--- client/containers/ProductsList.jsx
+++ client/containers/ProductsList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, withRouter } from 'react-router-dom';
 import * as actions from '../actions';
 import ProductsListItem from '../components/ProductsListItem';
 import ProductDetail from './ProductDetail';
@@ -11,33 +11,34 @@ class ProductsList extends Component {
     super(props);
     this.state = {
       cart: false,
+      length: this.props.productsList.length,
     };
     this.handleClick = this.handleClick.bind(this);
   }
-
   componentDidMount() {
     this.props.getProductsList();
   }
 
   handleClick(id) {
+    document.getElementById(id).setAttribute('disabled', true);
+    console.log(document.getElementById(id));
     this.props.addToCart(id);
   }
 
   render() {
-    console.log('match', this.props.match);
     const renderedList = this.props.productsList.map((item) => {
       return (
         <li key={item._id}>
-          <Link to={`/products/${item._id}`}><ProductsListItem id={item._id} image={item.image} name={item.name} cart={this.state.cart} /></Link>
-          <button className="add-to-cart" onClick={() => { this.handleClick(item._id); }} > ADD TO CART
+          <Link to={`/products/${item._id}`}><ProductsListItem id={item._id} image={item.image} name={item.name} price={item.price} cart={this.state.cart} /></Link>
+          <button id={item._id} className="add-to-cart" onClick={() => { this.handleClick(item._id); }}> Add to Cart
           </button>
         </li>
       );
     });
     return (
-      <div className="container">
-        <Route path={`${this.props.match.url}/:itemId`} component={ProductDetail} />
+      <div className="products-container">
         {/* Wrap the contents inside parent route. If url matches exactly to /products render list of products */}
+        <Route path={`${this.props.match.url}/:itemId`} component={ProductDetail} />
         <Route exact path={this.props.match.url} render={() => (
           <div className="products-content">
             <ul className="products-list">
@@ -61,4 +62,4 @@ ProductsList.propTypes = {
   match: PropTypes.object,
 };
 
-export default connect(mapStateToProps, actions)(ProductsList);
+export default withRouter(connect(mapStateToProps, actions)(ProductsList));
